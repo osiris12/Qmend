cae84308fd5ae958ba768f884e2650d921390c60
Osiris Robledo <robledoosiris@gmail.com>
2022-02-21 20:58:12 -0600

    Create City model and migration

---
+++ database/migrations/2022_02_22_021519_create_cities_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 35);
+            $table->string('state_id', 2);
+            $table->string('state_name', 20);
+            $table->unsignedInteger('county_flips', 11);
+            $table->string('county_name', 21);
+            $table->decimal('lat', 7, 4);
+            $table->decimal('lng', 7, 4);
+            $table->unsignedInteger('population');
+            $table->unsignedInteger('density');
+            $table->string('timezone', 30);
+            $table->string('zips', 1859);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
