67affb804eb52f1aa7377a9d0a343acf3c6072c9
Osiris Robledo <robledoosiris@gmail.com>
2022-02-26 16:40:41 -0600

    Add business and review seeders to DatabaseSeeder.

--- database/seeders/ReviewsSeeder.php
+++ database/seeders/ReviewsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class ReviewsSeeder extends Seeder
@@ -13,6 +14,6 @@ class ReviewsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Review::factory()->count(100)->create();
     }
 }
