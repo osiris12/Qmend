dfc67c58164349090585cfa8357a5f0a30eeb11a
DESKTOP-QAH2V8H\osiri <robledoosiris@gmail.com>
2022-08-18 13:11:13 -0500

    Change source to source_id in factory. Add use statement in migration.

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -22,8 +22,8 @@ class ReviewFactory extends Factory
     public function definition()
     {
         return [
-            'business_id' => $this->faker->numberBetween(1, 50),
-            'source' => $this->faker->numberBetween(1, 5),
+            'business_id' => $this->faker->numberBetween(1, 5000),
+            'source_id' => $this->faker->numberBetween(1, 5),
             'number_of_reviews' => $this->faker->numberBetween(100, 2000),
             'score' => $this->faker->randomFloat(1, 1, 5),
             'link' => 'https://www.google.com/maps/place/Siena+Tavern/@41.8890864,-87.6298924,15z/data=!3m1!5s0x880e2cb74cae0e7b:0xb7392f617178520d!4m7!3m6!1s0x0:0xecd6f9b48656e27b!8m2!3d41.8890864!4d-87.6298924!9m1!1b1',
